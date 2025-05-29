f0330ed81c77d2ecc85f29bca579abd8e31e6be7
istasiuk <uliastasuk118@gmail.com>
2025-05-29 22:52:26 +0100

    filter by petition

--- petitions_app.py
+++ petitions_app.py
@@ -169,10 +169,10 @@ for col in date_columns:
 pagination_cols = st.columns([1, 5, 1, 1, 1, 1])
 
 with pagination_cols[0]:
+    st.markdown(f"### Showing page **{st.session_state.page}** of **{total_pages}** ({total_items:,} total)")
+with pagination_cols[1]:
     if st.button("⏮ First"):
         st.session_state.page = 1
-with pagination_cols[1]:
-    st.markdown(f"### Showing page **{st.session_state.page}** of **{total_pages}** ({total_items:,} total)")
 with pagination_cols[2]:
     if st.button("◀ Prev") and st.session_state.page > 1:
         st.session_state.page -= 1
