5912e77ab4d8ef65e47f05930b2961eb75977b2c
istasiuk <uliastasuk118@gmail.com>
2025-05-30 20:30:18 +0100

    filter by signatures

--- petitions_app.py
+++ petitions_app.py
@@ -100,12 +100,10 @@ with st.sidebar:
     state_filter = st.multiselect("State", options=state_options, default=[])
     department_filter = st.multiselect("Department", options=department_options, default=[])
 
-    st.subheader("Signatures")
-
     max_signatures = int(df["Signatures"].max()) if not df["Signatures"].isnull().all() else 0
     min_signatures = int(df["Signatures"].min()) if not df["Signatures"].isnull().all() else 0
 
-    min_possible = min_signatures
+    min_possible = 0
     max_possible = max_signatures
 
     col1, col2 = st.columns(2)  # create two columns
